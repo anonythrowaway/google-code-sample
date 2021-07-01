5d963ef354a4b80ef29198fec83714610d12f681
Ayushman Singh <singhayushman13@protonmail.com>
2021-07-01 16:38:48 +0530

    add my solution

--- python/src/video.py
+++ python/src/video.py
@@ -10,6 +10,8 @@ class Video:
         """Video constructor."""
         self._title = video_title
         self._video_id = video_id
+        self._flagged = False
+        self.__flag_reason = None
 
         # Turn the tags into a tuple here so it's unmodifiable,
         # in case the caller changes the 'video_tags' they passed to us
@@ -29,3 +31,36 @@ class Video:
     def tags(self) -> Sequence[str]:
         """Returns the list of tags of a video."""
         return self._tags
+
+    @property
+    def flagged(self):
+        return self._flagged
+
+    @property
+    def flag_reason(self):
+        return self.__flag_reason
+
+    # @flagged.setter
+    def set_flagged(self, flag, flag_reason='Not supplied'):
+        if self._flagged and flag:
+            print('Cannot flag video: Video is already flagged')
+        elif self._flagged:
+            self._flagged = flag
+            self.__flag_reason = None
+            print(f'Successfully removed flag from video: {self.title}')
+        elif flag:
+            self._flagged = flag
+            self.__flag_reason = flag_reason
+            print(f'Successfully flagged video: {self.title} (reason: {flag_reason})')
+        else:
+            print('Cannot remove flag from video: Video is not flagged')
+
+    def __str__(self):
+        title, video_id, tags = self.title, self.video_id, self.tags
+        tags = ' '.join(tags)
+        video_str = f'{title} ({video_id}) [{tags}]'
+
+        if self._flagged:
+            video_str = f'{video_str} - FLAGGED (reason: {self.__flag_reason})'
+
+        return video_str
--- python/src/video_playlist.py
+++ python/src/video_playlist.py
@@ -1,5 +1,38 @@
 """A video playlist class."""
 
-
 class Playlist:
     """A class used to represent a Playlist."""
+    def __init__(self, name):
+        self._name = name
+        self._videos = list()
+    
+    @property
+    def name(self):
+        return self._name
+
+    def add_video(self, video, playlist_name):
+        if video not in self._videos:
+            self._videos.append(video)
+            message = f'Added video to {playlist_name}: {video.title}'
+        else:
+            message = f'Cannot add video to {playlist_name}: Video already added'
+        
+        print(message)
+
+    def show_playlist(self):
+        if len(self._videos) > 0:
+            for video in self._videos:
+                print(str(video))
+        else:
+            print('No videos here yet')
+
+    def remove_video(self, video, playlist_name):
+        try:
+            self._videos.remove(video)
+            print(f'Removed video from {playlist_name}: {video.title}')
+        except ValueError:
+            print(f'Cannot remove video from {playlist_name}: Video is not in playlist')
+
+    def clear_playlist(self, playlist_name):
+        self._videos.clear()
+        print(f'Successfully removed all videos from {playlist_name}')
